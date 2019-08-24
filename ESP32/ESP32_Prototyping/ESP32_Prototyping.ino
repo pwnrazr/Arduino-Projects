c378bae166881de0a991bfe98e1d6e2393e317e6
Amir Azmi <amirz98@hotmail.com>
2019-08-24 16:49:03 +0800

    Added ESP32_Prototyping

---
+++ ESP32/ESP32_Prototyping/ESP32_Prototyping.ino
@@ -0,0 +1,36 @@
+/*
+ * just playing around with the ESP32
+ * 
+ */
+
+#include "ota.h"
+
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+const char* ssid = "Ayam Goreng";
+const char* password = "pwnrazr1234";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  otaSetup();
+  
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  ArduinoOTA.handle();
+}
